6c864e0403e993ffa0d0223f20a98da138cdff95
BaohuiDeng <46564403+BaohuiDeng@users.noreply.github.com>
2020-05-24 15:47:22 +0200

    Update login_example.py

--- login_example.py
+++ login_example.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo 
+import bcrypt
 
 app = Flask(__name__)
 
@@ -10,8 +11,8 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    if 'username' in session:
-        return 'You are logged in as ' + session['username']
+    #if 'username' in session:
+     #   return 'You are logged in as ' + session['username']
 
     return render_template('index.html')
 
@@ -21,9 +22,9 @@ def login():
     login_user = users.find_one({'name' : request.form['username']})
 
     if login_user:
-        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
             session['username'] = request.form['username']
-            return redirect(url_for('index'))
+            return 'You are logged in as ' + session['username']
 
     return 'Invalid username/password combination'
 
@@ -34,10 +35,10 @@ def register():
         existing_user = users.find_one({'name' : request.form['username']})
 
         if existing_user is None:
-            hashpass = (request.form['pass'].encode('utf-8'))
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
             users.insert({'name' : request.form['username'], 'password' : hashpass})
             session['username'] = request.form['username']
-            return redirect(url_for('index'))
+            return 'you are successfully registered'
         
         return 'That username already exists!'
 
